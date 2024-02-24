556678b32c0795b9003661ca7334b22bf7ff7a8c
fnt4sia <fitraramadhan668@gmail.com>
2024-02-24 22:17:52 +0700

    Create Production QR Code Link

--- src/index.js
+++ src/index.js
@@ -11,6 +11,26 @@ const router = createBrowserRouter([
     path: "/",
     element: <Home />,
   },
+  {
+    path: "/fragrance",
+    element: <h1>Hello Fragrance</h1>
+  },
+  {
+    path: "/bodymist",
+    element: <h1>Hello BodyMist</h1>
+  },
+  {
+    path: "/bodymist/vanilla-milk",
+    element: <h1>Hello Fragrance</h1>
+  },
+  {
+    path: "/bodymist/chocolate-cookies",
+    element: <h1>Hello Fragrance</h1>
+  },
+  {
+    path: "/bodymist/cheese-cake",
+    element: <h1>Hello Fragrance</h1>
+  },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
